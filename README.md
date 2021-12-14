f8b92f94f1bafd10d15ec860ce3658e4ae766870
badre daha belghiti <bdahabelghiti.ext@louvre-hotels.com>
2021-12-14 14:21:16 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# rn-adyen
+
+React native module for Adyen DropIn
+
+## Installation
+
+```sh
+npm install rn-adyen
+```
+
+## Usage
+
+```js
+import { multiply } from "rn-adyen";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/rnadyen/RnAdyenPackage.kt
@@ -0,0 +1,17 @@
+package com.rnadyen
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class RnAdyenPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(RnAdyenModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
